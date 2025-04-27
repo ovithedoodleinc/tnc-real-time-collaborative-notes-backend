63477b6fb3912d56c94d7f3e0e68eb8dc2600942
Showvike Mondal Ovi <ovi.thedoodleinc@gmail.com>
2025-04-27 17:50:23 +0600

    nodemon, dotenv installed & configured

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const dotenv = require("dotenv");
 const express = require("express");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.send("hello, world!");
+  res.send(`hello, world! on port ${PORT}`);
 });
 
-app.listen(5000, () => {
-  console.log("🚀 ~ app.listen ~ running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`🚀 ~ app.listen ~ running on http://localhost:${PORT}`);
 });
